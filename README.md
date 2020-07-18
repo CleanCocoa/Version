638cc06dcfa4acc2b423cf4c9484cd162a4516bf
Christian Tietze <me@christiantietze.de>
2020-07-18 12:14:09 +0200

    import Version code

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Version
 
-A description of this package.
+Swift µ-)library to represent (semantic) Versions in code, including parsing from String.
+
+## Usage
+
+```swift
+import Version
+
+let version = Version(2, 3, 4)
+print(version) // => "2.3.4"
+
+let update = Version(fromString: "2.3.5")
+print(update.major) // => "5"
+print(update.minor) // => "5"
+print(update.patch) // => "5"
+```
+## License
+
+ Copyright © 2020 Christian Tietze. All rights reserved. Distributed under the MIT License.
--- Tests/VersionTests/VersionTests.swift
+++ Tests/VersionTests/VersionTests.swift
@@ -1,15 +1,48 @@
+//  Copyright © 2018 Christian Tietze. All rights reserved. Distributed under the MIT License.
+
 import XCTest
 @testable import Version
 
-final class VersionTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Version().text, "Hello, World!")
+class VersionTests: XCTestCase {
+    static var allTests = [
+        ("testComparison", testComparison),
+        ("testInitFromString", testInitFromString)
+    ]
+
+    func testComparison() {
+        XCTAssertEqual(Version(3,2,1), Version(3,2,1))
+
+        XCTAssert(Version(1,0,0) < Version(2,0,0))
+        XCTAssert(Version(1,0,0) < Version(1,1,1))
+        XCTAssert(Version(1,0,0) < Version(1,1,0))
+        XCTAssert(Version(1,0,0) < Version(1,0,1))
+
+        XCTAssert(Version(1,0,1) < Version(2,0,0))
+        XCTAssert(Version(1,0,1) < Version(1,0,2))
+        XCTAssert(Version(1,0,1) < Version(1,1,0))
+        XCTAssert(Version(1,0,1) < Version(1,1,1))
+        XCTAssert(Version(1,0,1) < Version(1,1,2))
+
+        XCTAssert(Version(1,1,0) < Version(2,0,0))
+        XCTAssert(Version(1,1,0) < Version(1,1,1))
+        XCTAssert(Version(1,1,0) < Version(1,2,0))
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    func testInitFromString() {
+        XCTAssertEqual(try Version(string: "2.3.4"), Version(2,3,4))
+        XCTAssertEqual(try Version(string: "2.1"), Version(2,1,0))
+        XCTAssertEqual(try Version(string: "2"), Version(2,0,0))
+        XCTAssertEqual(try Version(string: "0.1.2"), Version(0,1,2))
+        XCTAssertEqual(try Version(string: "3"), Version(3,0,0))
+        XCTAssertEqual(try Version(string: "0"), Version(0,0,0))
+
+        XCTAssertEqual(try Version(string: "-1"), Version(1,0,0))
+        XCTAssertEqual(try Version(string: "-1.0.0"), Version(1,0,0))
+        XCTAssertEqual(try Version(string: "2.-1.0"), Version(2,1,0))
+        XCTAssertEqual(try Version(string: "2.3.-4"), Version(2,3,4))
+        XCTAssertEqual(try Version(string: "7.8.9.1"), Version(7,8,9))
+
+        XCTAssertThrowsError(try Version(string: "x"))
+        XCTAssertThrowsError(try Version(string: "x.y.z"))
+    }
 }
